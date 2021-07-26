ea33f3fe117baa3dbdee88bd6deef0ff953c78de
gold-olar <sam99kupo@gmail.com>
2021-07-27 00:25:27 +0100

    chore: add landing page UI

--- src/pages/homepage/index.js
+++ src/pages/homepage/index.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Footer from "../../components/Footer";
+import Features from "../../components/LandingPageComponents/Features";
+import Header from "../../components/LandingPageComponents/Header";
+import Newsletter from "../../components/LandingPageComponents/NewsLetter";
 
 const Homepage = () => {
-  return <>Landing page goes here</>;
+  return (
+    <>
+      <Header />
+      <Features />
+      <Newsletter />
+      <Footer />
+    </>
+  );
 };
 
 export default Homepage;
