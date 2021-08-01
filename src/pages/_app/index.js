49ee605515d978acf36d541b2b9e0eae87c02760
Steven Li <steven4354@gmail.com>
2021-08-01 01:51:13 -0700

    detail page route

--- src/pages/_app/index.js
+++ src/pages/_app/index.js
@@ -4,6 +4,7 @@ import Homepage from "../homepage";
 import NotFoundPage from "../not-found";
 import ContactPage from "../contact";
 import ListingsPage from "../listings";
+import DetailPage from "../detail";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
           <Route path="/" exact component={ListingsPage} />
           <Route path="/contact" exact component={ContactPage} />
           <Route path="/listings/:location" exact component={ListingsPage} />
+          <Route path="/detail/:id" exact component={DetailPage} />
           <Route path="*" component={NotFoundPage} />
         </Switch>
       </Router>
---
+++ src/pages/detail/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ContactForm from "../../components/ContactForm";
+
+// See: https://liquidspace.com/us/ca/san-francisco/shared/monthly-weekend-evening#wp-monthly 
+// For example on how this page should look
+const DetailPage = () => {
+  return <>place detail page here</>;
+};
+
+export default DetailPage;
