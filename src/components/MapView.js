e3c730716367cb9107b66a81e595c13b46046400
Steven Li <steven4354@gmail.com>
2021-08-01 00:14:37 -0700

    adding map view layout & header

---
+++ src/components/MapView.js
@@ -0,0 +1,136 @@
+import React, { PureComponent } from 'react';
+import ReactMapGL, { Marker } from 'react-map-gl';
+import Geocoder from 'react-mapbox-gl-geocoder';
+import { Container, Col, Row, Button } from 'reactstrap';
+
+// const mapStyle = {
+//     width: '100%',
+//     height: 800
+// }
+
+const mapboxApiKey = 'pk.eyJ1Ijoic3RldmVuNDM1NCIsImEiOiJjazhwZzg5djcxZjFlM21renZzMmIyZGNmIn0.2fVgMsjPnEs3g_7vF6Rz9g'
+
+const params = {
+    country: "ca"
+}
+
+const CustomMarker = ({index, marker}) => {
+  return (
+    <Marker
+      longitude={marker.longitude}
+      latitude={marker.latitude}>
+      <div className="marker">
+        <span><b>{index + 1}</b></span>
+      </div>
+    </Marker>
+  )
+};
+
+function getWindowDimensions() {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+      width,
+      height
+    };
+  }
+
+class MapView extends PureComponent {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewport: {
+        latitude: 45.50884,
+        longitude: -73.58781,
+        zoom: 15
+      },
+      tempMarker: null,
+      markers:[]
+    };
+
+    const { width, height } = getWindowDimensions()
+
+    this.mapStyle = {
+        width: '100%',
+        height: height - 55
+    }
+  }
+
+  onSelected = (viewport, item) => {
+      this.setState({
+        viewport,
+        tempMarker: {
+          name: item.place_name,
+          longitude: item.center[0],
+          latitude: item.center[1]
+        }
+      })
+  }
+
+  add = () => {
+    var { tempMarker } = this.state
+
+    this.setState(prevState => ({
+        markers: [...prevState.markers, tempMarker],
+        tempMarker: null
+    }))
+  }
+
+  render() {
+    const { viewport, tempMarker, markers } = this.state;
+    return(
+      <Container fluid={true}>
+        {/* <Row>
+          <Col><h2>Mapbox Tutorial</h2></Col>
+        </Row> */}
+        {/* <Row className="py-4">
+          <Col xs={2}>
+            <Geocoder
+                mapboxApiAccessToken={mapboxApiKey}
+                onSelected={this.onSelected}
+                viewport={viewport}
+                hideOnSelect={true}
+                value=""
+                queryParams={params}
+            />
+          </Col>
+          <Col>
+           <Button color="primary" onClick={this.add}>Add</Button>
+          </Col>
+        </Row> */}
+        <Row>
+          <Col>
+            <ReactMapGL
+              mapboxApiAccessToken={mapboxApiKey}
+              mapStyle="mapbox://styles/mapbox/streets-v11"
+              {...viewport}
+              {...this.mapStyle}
+              onViewportChange={(viewport) => this.setState({viewport})}
+            >
+              { tempMarker &&
+                <Marker
+                  longitude={tempMarker.longitude}
+                  latitude={tempMarker.latitude}>
+                  <div className="marker temporary-marker"><span></span></div>
+                </Marker>
+              }
+              {
+                this.state.markers.map((marker, index) => {
+                  return(
+                    <CustomMarker
+                      key={`marker-${index}`}
+                      index={index}
+                      marker={marker}
+                    />
+                  )
+                })
+              }
+            </ReactMapGL>
+          </Col>
+        </Row>
+      </Container>
+   );
+  }
+}
+
+export default MapView;
--- src/pages/homepage/index.js
+++ src/pages/homepage/index.js
@@ -1,7 +1,45 @@
 import React from "react";
+import Header from "../../components/Header"
+import MapView from "../../components/MapView"
 
-const Homepage = () => {
-  return <>Landing page goes here</>;
-};
+/* This example requires Tailwind CSS v2.0+ */
+import { useState } from 'react'
 
-export default Homepage;
+export default function Example() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
+  return (
+    <>
+      <Header />
+      <div className="h-screen flex bg-gray-50 overflow-hidden">
+        <div className="flex-1 min-w-0 flex flex-col overflow-hidden">
+          <main className="flex-1 flex overflow-hidden">
+            <div className="flex-1 flex xl:overflow-hidden">
+              {/* Primary column */}
+              <section
+                aria-labelledby="primary-heading"
+                className="min-w-0 flex-1 h-full flex flex-col overflow-hidden lg:order-last"
+              >
+                <h1 id="primary-heading" className="sr-only">
+                  Account
+                </h1>
+                {/* Your content */}
+                add listings here
+              </section>
+
+              {/* Secondary column (hidden on smaller screens) */}
+              <aside className="hidden lg:block lg:flex-shrink-0 lg:order-first">
+                {/* TODO: remove custom 28 rem with a tailwind class like w-96 */}
+                <div className="h-full relative flex flex-col border-r border-gray-200 bg-white" style={{ width: '28rem'}}>
+                  {/* Your content */}
+                  <MapView />
+                </div>
+              </aside>
+            </div>
+          </main>
+        </div>
+      </div>
+    </>
+  )
+}
+
