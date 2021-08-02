d8cbd865e464742e63799c83186a15a077eefdb2
Steven Li <steven4354@gmail.com>
2021-08-01 21:41:15 -0700

    added back some images to details page

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -29,7 +29,7 @@ export default function Example() {
           <div class="md:w-96 px-0.5 mb-2"><img class="shadow-md" src="https://source.unsplash.com/random/1280x720" alt=""/></div>
         </div>
       </div> */}
-      {/* <section class="flex pt-4 px-4 overflow-x-scroll overflow-y-hidden" style={{maxHeight: 400}}>
+      <section class="flex pt-4 px-4 overflow-x-scroll overflow-y-hidden" style={{maxHeight: 400}}>
         <div class="flex flex-wrap -mx-4 overflow-x-scroll">
           <div class="md:w-1/2 px-0.5 mb-2"><img class="shadow-md" src="https://source.unsplash.com/random/1280x720" alt=""/></div>
           <div class="md:w-1/2 px-0.5 mb-2"><img class="shadow-md" src="https://source.unsplash.com/random/1280x720" alt=""/></div>
@@ -38,7 +38,7 @@ export default function Example() {
           <div class="md:w-1/2 px-0.5 mb-2"><img class="shadow-md" src="https://source.unsplash.com/random/1280x720" alt=""/></div>
           <div class="md:w-1/2 px-0.5 mb-2"><img class="shadow-md" src="https://source.unsplash.com/random/1280x720" alt=""/></div>
         </div>
-      </section> */}
+      </section>
       {/* <div class="h-64 grid grid-rows-2 grid-flow-col gap-4 overflow-x-scroll">
           <div class="md:w-96 px-0.5 mb-2"><img class="shadow-md" src="https://source.unsplash.com/random/1280x720" alt=""/></div>
           <div class="md:w-96 px-0.5 mb-2"><img class="shadow-md" src="https://source.unsplash.com/random/1280x720" alt=""/></div>
@@ -52,11 +52,11 @@ export default function Example() {
           <main className="lg:col-span-9 xl:col-span-8">
             <div className="sm:px-3">
               <h2 className="text-2xl font-normal leading-7 text-gray-900 sm:text-3xl sm:truncate">Open Storage - 30 Available at The Hive Lavender</h2>
-              <p class="mt-2 text-sm font-thin text-gray-400 sm:text-base sm:truncate">1 Kallang Junction Singapore 339263, Singapore, Singapore 339263 - Confirmed Open</p>
+              <p class="mt-2 text-sm font-light text-gray-400 sm:text-base sm:truncate">1 Kallang Junction Singapore 339263, Singapore, Singapore 339263 - Confirmed Open</p>
             </div>
             <div className="mt-12 sm:px-3">
               <h2 className="text-base font-normal leading-7 text-gray-900 sm:text-base sm:truncate">24/7 Storage - 15x15 Space Size</h2>
-              <p class="mt-2 text-sm font-thin text-gray-400 sm:text-base">The Hive Lavender combines coworking, beautiful work space, fantastic amenities and meeting rooms, with a Hive Café at the centre of the space surrounded by hotdesks, booth desks, 2 to 32 person private offices. Plus a full-sized fashion photography studio, portrait studio, You Tube Studio, plus a 3D printing & prototyping workshop all under the same roof.</p>
+              <p class="mt-2 text-sm font-light text-gray-400 sm:text-base">The Hive Lavender combines coworking, beautiful work space, fantastic amenities and meeting rooms, with a Hive Café at the centre of the space surrounded by hotdesks, booth desks, 2 to 32 person private offices. Plus a full-sized fashion photography studio, portrait studio, You Tube Studio, plus a 3D printing & prototyping workshop all under the same roof.</p>
             </div>
           </main>
           <aside className="hidden xl:block xl:col-span-4">
