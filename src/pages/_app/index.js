5dc22d282e2fd1c64295edb918b07d216200cee2
Steven Li <steven4354@gmail.com>
2021-08-01 17:56:11 -0700

    testing image scroll views for detail page

--- src/pages/_app/index.js
+++ src/pages/_app/index.js
@@ -5,6 +5,7 @@ import NotFoundPage from "../not-found";
 import ContactPage from "../contact";
 import ListingsPage from "../listings";
 import DetailPage from "../detail";
+import TertiaryColumn from "../../samples/tertiary-column"
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
           <Route path="/contact" exact component={ContactPage} />
           <Route path="/listings/:location" exact component={ListingsPage} />
           <Route path="/detail/:id" exact component={DetailPage} />
+          <Route path="/sample" exact component={TertiaryColumn} />
           <Route path="*" component={NotFoundPage} />
         </Switch>
       </Router>
--- src/samples/tertiary-column.js
+++ src/samples/tertiary-column.js
@@ -1,6 +1,30 @@
+/*
+  This example requires Tailwind CSS v2.0+ 
+  
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  const colors = require('tailwindcss/colors')
+  
+  module.exports = {
+    // ...
+    theme: {
+      extend: {
+        colors: {
+          rose: colors.rose,
+        }
+      }
+    },
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ]
+  }
+  ```
+*/
 import { Fragment } from 'react'
 import { Menu, Popover, Transition } from '@headlessui/react'
-import Header from '../../components/Header'
 import {
   ChatAltIcon,
   CodeIcon,
@@ -101,33 +125,52 @@ function classNames(...classes) {
 export default function Example() {
   return (
     <div className="min-h-screen bg-gray-100">
-      <Header />
-      <div
-        class="flex overflow-x-scroll pb-10 hide-scroll-bar"
-      >
-        <div
-          class="flex flex-nowrap lg:ml-40 md:ml-20 ml-10 "
-        >
-          <div class="md:w-96 px-0.5 mb-2"><img class="shadow-md" src="https://source.unsplash.com/random/1280x720" alt=""/></div>
-          <div class="md:w-96 px-0.5 mb-2"><img class="shadow-md" src="https://source.unsplash.com/random/1280x720" alt=""/></div>
-          <div class="md:w-96 px-0.5 mb-2"><img class="shadow-md" src="https://source.unsplash.com/random/1280x720" alt=""/></div>
-          <div class="md:w-96 px-0.5 mb-2"><img class="shadow-md" src="https://source.unsplash.com/random/1280x720" alt=""/></div>
-        </div>
-      </div>
-      <section class="flex flex-row pt-4 px-4 overflow-x-scroll overflow-y-hidden" style={{maxHeight: 400}}>
-        <div class="flex flex-row -mx-4 overflow-x-scroll">
-          <div class="md:w-1/2 px-0.5 mb-2"><img class="shadow-md" src="https://source.unsplash.com/random/1280x720" alt=""/></div>
-          <div class="md:w-1/2 px-0.5 mb-2"><img class="shadow-md" src="https://source.unsplash.com/random/1280x720" alt=""/></div>
-          <div class="md:w-1/2 px-0.5 mb-2"><img class="shadow-md" src="https://source.unsplash.com/random/1280x720" alt=""/></div>
-          <div class="md:w-1/2 px-0.5 mb-2"><img class="shadow-md" src="https://source.unsplash.com/random/1280x720" alt=""/></div>
-          <div class="md:w-1/2 px-0.5 mb-2"><img class="shadow-md" src="https://source.unsplash.com/random/1280x720" alt=""/></div>
-          <div class="md:w-1/2 px-0.5 mb-2"><img class="shadow-md" src="https://source.unsplash.com/random/1280x720" alt=""/></div>
-        </div>
-      </section>
       <div className="py-10">
         <div className="max-w-3xl mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-12 lg:gap-8">
           <main className="lg:col-span-9 xl:col-span-8">
-            <div>
+            <div className="px-4 sm:px-0">
+              <div className="sm:hidden">
+                <label htmlFor="question-tabs" className="sr-only">
+                  Select a tab
+                </label>
+                <select
+                  id="question-tabs"
+                  className="block w-full rounded-md border-gray-300 text-base font-medium text-gray-900 shadow-sm focus:border-rose-500 focus:ring-rose-500"
+                  defaultValue={tabs.find((tab) => tab.current).name}
+                >
+                  {tabs.map((tab) => (
+                    <option key={tab.name}>{tab.name}</option>
+                  ))}
+                </select>
+              </div>
+              <div className="hidden sm:block">
+                <nav className="relative z-0 rounded-lg shadow flex divide-x divide-gray-200" aria-label="Tabs">
+                  {tabs.map((tab, tabIdx) => (
+                    <a
+                      key={tab.name}
+                      href={tab.href}
+                      aria-current={tab.current ? 'page' : undefined}
+                      className={classNames(
+                        tab.current ? 'text-gray-900' : 'text-gray-500 hover:text-gray-700',
+                        tabIdx === 0 ? 'rounded-l-lg' : '',
+                        tabIdx === tabs.length - 1 ? 'rounded-r-lg' : '',
+                        'group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-6 text-sm font-medium text-center hover:bg-gray-50 focus:z-10'
+                      )}
+                    >
+                      <span>{tab.name}</span>
+                      <span
+                        aria-hidden="true"
+                        className={classNames(
+                          tab.current ? 'bg-rose-500' : 'bg-transparent',
+                          'absolute inset-x-0 bottom-0 h-0.5'
+                        )}
+                      />
+                    </a>
+                  ))}
+                </nav>
+              </div>
+            </div>
+            <div className="mt-4">
               <h1 className="sr-only">Recent questions</h1>
               <ul className="space-y-4">
                 {questions.map((question) => (
