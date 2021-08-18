3765e3a77bad28351d7bfbda8653b6440e4b458a
Arjun Nair <nair.arjun47@yahoo.com>
2021-08-18 13:11:21 -0700

    Error in ListView
    There is an error in ListView on line 128 where it cannot read the id of undefied.
    
    I tried multiple solutions but feel like I am overthinking it.
    
    I also made a seperate dir and file for the API but since I included it in the .gitignore, I will send a screenshot just in case that turns out to be the problem.

--- src/components/ListsView.js
+++ src/components/ListsView.js
@@ -1,144 +1,147 @@
 /* This example requires Tailwind CSS v2.0+ */
 import { withRouter } from "react-router-dom";
+import base from '../api/base';
 
-const people = [
-  {
-    name: 'Jane Cooper',
-    title: 'Paradigm Representative',
-    role: 'Admin',
-    email: 'janecooper@example.com',
-    telephone: '+1-202-555-0170',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-  },
-  {
-    name: 'Jane Cooper',
-    title: 'Paradigm Representative',
-    role: 'Admin',
-    email: 'janecooper@example.com',
-    telephone: '+1-202-555-0170',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-  },
-  {
-    name: 'Jane Cooper',
-    title: 'Paradigm Representative',
-    role: 'Admin',
-    email: 'janecooper@example.com',
-    telephone: '+1-202-555-0170',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-  },
-  {
-    name: 'Jane Cooper',
-    title: 'Paradigm Representative',
-    role: 'Admin',
-    email: 'janecooper@example.com',
-    telephone: '+1-202-555-0170',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-  },
-  {
-    name: 'Jane Cooper',
-    title: 'Paradigm Representative',
-    role: 'Admin',
-    email: 'janecooper@example.com',
-    telephone: '+1-202-555-0170',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-  },
-  {
-    name: 'Jane Cooper',
-    title: 'Paradigm Representative',
-    role: 'Admin',
-    email: 'janecooper@example.com',
-    telephone: '+1-202-555-0170',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-  },
-  {
-    name: 'Jane Cooper',
-    title: 'Paradigm Representative',
-    role: 'Admin',
-    email: 'janecooper@example.com',
-    telephone: '+1-202-555-0170',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-  },
-  {
-    name: 'Jane Cooper',
-    title: 'Paradigm Representative',
-    role: 'Admin',
-    email: 'janecooper@example.com',
-    telephone: '+1-202-555-0170',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-  },
-  {
-    name: 'Jane Cooper',
-    title: 'Paradigm Representative',
-    role: 'Admin',
-    email: 'janecooper@example.com',
-    telephone: '+1-202-555-0170',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-  },
-  {
-    name: 'Jane Cooper',
-    title: 'Paradigm Representative',
-    role: 'Admin',
-    email: 'janecooper@example.com',
-    telephone: '+1-202-555-0170',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-  },
-  {
-    name: 'Jane Cooper',
-    title: 'Paradigm Representative',
-    role: 'Admin',
-    email: 'janecooper@example.com',
-    telephone: '+1-202-555-0170',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-  },
-  {
-    name: 'Jane Cooper',
-    title: 'Paradigm Representative',
-    role: 'Admin',
-    email: 'janecooper@example.com',
-    telephone: '+1-202-555-0170',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-  },
-  {
-    name: 'Jane Cooper',
-    title: 'Paradigm Representative',
-    role: 'Admin',
-    email: 'janecooper@example.com',
-    telephone: '+1-202-555-0170',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-  },
-  // More people...
-]
+// const people = [
+//   {
+//     name: 'Jane Cooper',
+//     title: 'Paradigm Representative',
+//     role: 'Admin',
+//     email: 'janecooper@example.com',
+//     telephone: '+1-202-555-0170',
+//     imageUrl:
+//       'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+//   },
+//   {
+//     name: 'Jane Cooper',
+//     title: 'Paradigm Representative',
+//     role: 'Admin',
+//     email: 'janecooper@example.com',
+//     telephone: '+1-202-555-0170',
+//     imageUrl:
+//       'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+//   },
+//   {
+//     name: 'Jane Cooper',
+//     title: 'Paradigm Representative',
+//     role: 'Admin',
+//     email: 'janecooper@example.com',
+//     telephone: '+1-202-555-0170',
+//     imageUrl:
+//       'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+//   },
+//   {
+//     name: 'Jane Cooper',
+//     title: 'Paradigm Representative',
+//     role: 'Admin',
+//     email: 'janecooper@example.com',
+//     telephone: '+1-202-555-0170',
+//     imageUrl:
+//       'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+//   },
+//   {
+//     name: 'Jane Cooper',
+//     title: 'Paradigm Representative',
+//     role: 'Admin',
+//     email: 'janecooper@example.com',
+//     telephone: '+1-202-555-0170',
+//     imageUrl:
+//       'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+//   },
+//   {
+//     name: 'Jane Cooper',
+//     title: 'Paradigm Representative',
+//     role: 'Admin',
+//     email: 'janecooper@example.com',
+//     telephone: '+1-202-555-0170',
+//     imageUrl:
+//       'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+//   },
+//   {
+//     name: 'Jane Cooper',
+//     title: 'Paradigm Representative',
+//     role: 'Admin',
+//     email: 'janecooper@example.com',
+//     telephone: '+1-202-555-0170',
+//     imageUrl:
+//       'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+//   },
+//   {
+//     name: 'Jane Cooper',
+//     title: 'Paradigm Representative',
+//     role: 'Admin',
+//     email: 'janecooper@example.com',
+//     telephone: '+1-202-555-0170',
+//     imageUrl:
+//       'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+//   },
+//   {
+//     name: 'Jane Cooper',
+//     title: 'Paradigm Representative',
+//     role: 'Admin',
+//     email: 'janecooper@example.com',
+//     telephone: '+1-202-555-0170',
+//     imageUrl:
+//       'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+//   },
+//   {
+//     name: 'Jane Cooper',
+//     title: 'Paradigm Representative',
+//     role: 'Admin',
+//     email: 'janecooper@example.com',
+//     telephone: '+1-202-555-0170',
+//     imageUrl:
+//       'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+//   },
+//   {
+//     name: 'Jane Cooper',
+//     title: 'Paradigm Representative',
+//     role: 'Admin',
+//     email: 'janecooper@example.com',
+//     telephone: '+1-202-555-0170',
+//     imageUrl:
+//       'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+//   },
+//   {
+//     name: 'Jane Cooper',
+//     title: 'Paradigm Representative',
+//     role: 'Admin',
+//     email: 'janecooper@example.com',
+//     telephone: '+1-202-555-0170',
+//     imageUrl:
+//       'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+//   },
+//   {
+//     name: 'Jane Cooper',
+//     title: 'Paradigm Representative',
+//     role: 'Admin',
+//     email: 'janecooper@example.com',
+//     telephone: '+1-202-555-0170',
+//     imageUrl:
+//       'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+//   },
+//   // More people...
+// ]
 
-export function ListView({history}) {
+function ListView({records}) {
+
+  const recordsID = records.id;
   return (
     <ul className="flex-1 grid grid-cols-1 gap-2 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-4 p-2 overflow-y-scroll">
-      {people.map((person) => (
+      {records.map((records) => (
         <li
-          key={person.email}
+          key={recordsID}
           className="col-span-1 flex flex-col text-center bg-white rounded-lg shadow divide-y divide-gray-200 rounded-md"
-          onClick={() => history.push("/detail/111-1111-1111")}
+          onClick={() => records.push("/detail/111-1111-1111")}
         >
           <div className="flex-1 flex flex-col">
-            <img className="w-full h-3/5 flex-shrink-0 mx-auto bg-black rounded-t-md" src={"https://i.ibb.co/yXVYkpV/storage-services.jpg"} alt="" />
+            <img className="w-full h-3/5 flex-shrink-0 mx-auto bg-black rounded-t-md" src={records.fields.pictures} alt="" />
             <div className="mt-3 mb-2 flex-1 flex flex-col justify-between">
             <div>
-                <h3 className="text-gray-900 text-base font-medium text-left px-3">$929/mo</h3>
-                <h3 className="mt-1 text-gray-500 text-sm font-light text-left px-3">15x15 space</h3>
+                <h3 className="text-gray-900 text-base font-medium text-left px-3">${records.fields.price_per_month}/mo</h3>
+                <h3 className="mt-1 text-gray-500 text-sm font-light text-left px-3">{records.fields.unit_size} space</h3>
             </div>
-            <h3 className="mt-3 text-gray-500 text-sm font-thin text-left px-3 sm:truncate">2222 Terra Dr. San Francisco, CA, USA, 95682</h3>
+            <h3 className="mt-3 text-gray-500 text-sm font-thin text-left px-3 sm:truncate">{records.fields.facility_address}</h3>
             </div>
           </div>
         </li>
